2801929ba7355f2dae969b024ce8f87d408ebea1
Aditya Bhatt <you@example.com>
2024-05-06 22:17:45 +0530

    added media queries

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-const popup = document.querySelector('.popup')
-const button = document.querySelector('.join-btn')
-const clsBtn = document.querySelector('.close-btn')
+// selecting classes
 
-button.addEventListener('click',()=>{
-    popup.classList.add('open')
-    console.log('opened')
+const popup = document.querySelector(".popup");
+const button = document.querySelector(".join-btn");
+const clsBtn = document.querySelector(".close-btn");
+// const username = document.getElementById("username");
+// const email = document.getElementById("userEmail");
+const submitBtn = document.getElementById("s-btn");
+
+// click event handler to select open class
+button.addEventListener("click", () => {
+  popup.classList.add("open");
+  console.log("opened");
+});
+
+// click handler to remove the open class
+clsBtn.addEventListener("click", (event) => {
+  popup.classList.remove("open");
+  event.preventDefault(); // to stop the search param
+  console.log("closed");
+});
+
+// function res() {
+//   console.log(`Welcome ${username.value} 🙌 you have successfully logged in with ${email}`);
+// }
+// submitBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   res()
+// });
+submitBtn.addEventListener('click',(e)=>{
+  e.window.localtion.href=form.data;
+  console.log('asd')
 })
 
-
-clsBtn.addEventListener('click',()=>{
-    popup.classList.replace('')
-    console.log('closed')
-})
